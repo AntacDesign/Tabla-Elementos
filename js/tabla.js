68a449f80327b0e3438a9d875557c05306824e2e
Antac <angello_04_09@hotmail.com>
2020-05-28 20:05:29 -0500

    Mostrar grupo al que pertenece listo

--- js/tabla.js
+++ js/tabla.js
@@ -3,18 +3,31 @@ const botones = document.querySelectorAll(".btn-element");
 
 /* creando la funcion */
 const seleccionado = function (evento) {
+    
     /*Este obtine la clases */
     let clases = this.className;
+    
     /*Este obtine por clase */
     let clase = this.classList[1];
+    
     /*Este obtine el id */
     let id = this.id;
+    
     /*Este obtine el contenido del elemento */
     let contenido = this.innerText;
+    
     /*salida en la consola */
-    console.log("el texto seleccionado es: " + id)
+    console.log("el texto seleccionado es: " + clase)
+    
+    /* seleccionado elemento */
     document.getElementById(id).classList.toggle("seleccionado");
+
+    /* Dando grupo a la vista */
     document.getElementById("elemento").classList.toggle(clase);
+    
+    /* Mostrando grupo */
+    document.getElementById(clase).classList.toggle("grupo-seleccionado");
+    
 }
 
 /* dando evento a la contante boton */
