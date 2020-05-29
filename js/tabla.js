ba6e3b4c8de9d65a26ac613a51ea37bd773d9133
Antac <angello_04_09@hotmail.com>
2020-05-28 19:48:55 -0500

    Botones seleccionados terminado

--- js/tabla.js
+++ js/tabla.js
@@ -1,15 +1,24 @@
+/* toma todos los elementos que tengan la misma clase */
 const botones = document.querySelectorAll(".btn-element");
 
-const seleccionado = function(evento)
-{
-    let clase = this.className;
+/* creando la funcion */
+const seleccionado = function (evento) {
+    /*Este obtine la clases */
+    let clases = this.className;
+    /*Este obtine por clase */
+    let clase = this.classList[1];
+    /*Este obtine el id */
     let id = this.id;
+    /*Este obtine el contenido del elemento */
     let contenido = this.innerText;
+    /*salida en la consola */
     console.log("el texto seleccionado es: " + id)
+    document.getElementById(id).classList.toggle("seleccionado");
+    document.getElementById("elemento").classList.toggle(clase);
 }
 
-botones.forEach(boton =>
-    {
-        boton.addEventListener("click",seleccionado)
-    }
+/* dando evento a la contante boton */
+botones.forEach(boton => {
+    boton.addEventListener("click", seleccionado)
+}
 );
